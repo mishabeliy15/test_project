a06023e7b6b82fa1f471ec54369c610472fa88f8
misha.beliy15 <misha.beliy15@gmail.com>
2018-09-21 19:35:55 +0300

    Add Limit for video (50 min)

--- bot.py
+++ bot.py
@@ -4,10 +4,11 @@ from my_parser import *
 import youtube_dl
 import os
 
-token = "464958591:AAG6_2iK9BePRuWyMvCg5vejzixlunfNz64"
+token = ""
 bot = telebot.TeleBot(token)
 
-N = 8
+N = 8 #Count of request
+
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
@@ -18,21 +19,32 @@ def handle_text(message):
         parse = Parsing(message.text)
         inlineKey = types.InlineKeyboardMarkup()
         row = []
-        for i in range(N):
+        n = N if N <= len(parse.videos) else len(parse.videos)
+        for i in range(n):
             print("[" + parse.req_search + " | " + parse.videos[i].name + "] " + parse.videos[i].url)
-            callback_button = types.InlineKeyboardButton(text=str(i + 1),callback_data=parse.videos[i].url)
+            callback_button = types.InlineKeyboardButton(text=str(i + 1), callback_data=parse.videos[i].url)
             row.append(callback_button)
         inlineKey.row(*row)
-        bot.send_message(message.chat.id, parse.get_names_to_str(N),reply_markup=inlineKey)
+        bot.send_message(message.chat.id, parse.get_names_to_str(n), reply_markup=inlineKey)
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     url = str(call.data)
     if url != '':
-        mp3(url, call.message,parse_name=True)
+        mp3(url, call.message, parse_name=True)
 
 
-def download_mp3_from_video(url,name):
+def mp3(link, message, parse_name=True):
+    name = get_name(link) if parse_name else "temp"
+    download_mp3_from_video(link, name)
+    audi = open(name + ".mp3", 'rb')
+    bot.send_audio(message.chat.id, audi)
+    audi.close()
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), name + '.mp3')
+    os.remove(path)
+
+def download_mp3_from_video(url, name):
     outtmpl = name + '.%(ext)s'
     ydl_opts = {
         'format': 'bestaudio/best',
@@ -45,15 +57,5 @@ def download_mp3_from_video(url,name):
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-
-def mp3(link, message,parse_name = True):
-    name = get_name(link) if parse_name else "temp"
-    download_mp3_from_video(link, name)
-    audi = open(name + ".mp3", 'rb')
-    bot.send_audio(message.chat.id, audi)
-    audi.close()
-    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), name + '.mp3')
-    os.remove(path)
-
 
 bot.polling(none_stop=True)
--- my_parser.py
+++ my_parser.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 class Parsing:
     youtube_search = 'https://www.youtube.com/results?sp=EgIQAQ%253D%253D&search_query='
 
     def __init__(self, req):
         self.req_search = req
-        self.site = BeautifulSoup(self.gethtml(self.youtube_search + req),'lxml')
+        self.site = BeautifulSoup(self.gethtml(self.youtube_search + req), 'lxml')
         self.videos = []
         self.parsing_videos()
         self.to_videos()
@@ -16,43 +17,58 @@ class Parsing:
         return r.text
 
     def parsing_videos(self):
-        self.blocks_a = self.site.select('a[class*="yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink spf-link"]')
+        self.blocks_a = self.site.select(
+            'a[class*="yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink spf-link"]')
+        self.times_span = self.site.select('span[class="accessible-description"]')
 
-    def get_names_to_str(self):
-        s = ""
-        count = 0
-        for i in self.blocks_a:
-            count += 1
-            s += str(count) + ") " + i.text + "\n"
-        return s
 
     def get_names_to_str(self, n):
         s = ''
         count = 0
-        for i in self.blocks_a:
+        for i in self.videos:
             count += 1
-            s += str(count) + ") " + i.text + "\n"
+            s += str(count) + ") " + i.name + "\n"
             if count == n:
                 break
         return s
 
     def to_videos(self):
-        for i in self.blocks_a:
-            temp = Video(i)
-            if len(temp.url) < 100:
+        j = 0
+        for i in range(len(self.blocks_a)):
+            temp = Video(self.blocks_a[i], self.times_span[j])
+            if (len(temp.url) < 100) and (temp.time_s < 60*50):
                 self.videos.append(temp)
+            if j + 1 < len(self.times_span):
+                j += 1
+
 
 youtube = 'https://www.youtube.com'
+
+
 class Video():
-    def __init__(self,soup):
+    def __init__(self, soup,soup_t):
         self.name = soup.text
         self.url = youtube + soup.attrs['href']
+        temp = soup_t.text.split(':')
+        time = 0
+        i = len(temp) - 1
+        temp[i] = temp[i][0:len(temp[i]) - 1]
+        multiple = 1
+        while i > 0:
+            time += int(temp[i])*multiple
+            multiple *= 60
+            i -= 1
+        self.time_s = time
 
     def __str__(self):
         return self.name
 
+
 def get_name(url):
-    soup = BeautifulSoup(requests.get(url).text,'lxml')
+    soup = BeautifulSoup(requests.get(url).text, 'lxml')
     name = soup.select('span[class*="watch-title"]')
-    name = name[0].attrs['title'].lower().replace(',', '_').replace('.', '_').replace(' ', '_')
+    name = name[0].attrs['title'].lower().replace(',', '_').replace('.', '_').replace(' ', '_').replace('-',
+                                                                                                        '_').replace('#', '')
+    name = name.replace('|','_')
+
     return name
